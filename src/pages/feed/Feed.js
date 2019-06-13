0f5f30bde80f0ea4c7ea81467a4ba2fdbb0227d8
Valdir Coxev <valdircoxevdev@gmail.com>
2019-06-13 11:30:58 -0400

    Adding component feed get posts

---
+++ src/pages/feed/Feed.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+import api from "../../services/api";
+
+import "./Feed.css";
+
+import more from "../../assets/more.svg";
+import like from "../../assets/like.svg";
+import comment from "../../assets/comment.svg";
+import send from "../../assets/send.svg";
+
+class Feed extends Component {
+  state = {
+    feed: []
+  };
+  async componentDidMount() {
+    const { data } = await api.get("posts");
+    this.setState({ feed: data });
+  }
+  render() {
+    const { feed } = this.state;
+    return (
+      <section id="post-list">
+        {feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="Mais" />
+            </header>
+            <img src={`http://localhost:3001/files/${post.image}`} alt="" />
+            <footer>
+              <div className="actions">
+                <img src={like} alt="like" />
+                <img src={comment} alt="comment" />
+                <img src={send} alt="send" />
+              </div>
+              <strong>{post.likes} curtidas</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
+      </section>
+    );
+  }
+}
+
+export default Feed;
